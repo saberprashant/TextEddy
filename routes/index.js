8ff854999d1af6757df017256c54111376813b01
saberprashant <prashantjain.pro@gmail.com>
2018-03-22 19:53:14 +0530

    files added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get("/server", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+router.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html').status(200);
 });
 module.exports = router;
